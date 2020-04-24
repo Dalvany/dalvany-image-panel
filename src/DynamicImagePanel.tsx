ad3561ef5464c0359953a0df9a7ad9c8cf183e3d
Dalvany <Dalvany@users.noreply.github.com>
2020-04-24 12:55:12 +0200

    Add an alt attribute
    Alt attribute contains the metric value

--- src/DynamicImagePanel.tsx
+++ src/DynamicImagePanel.tsx
@@ -34,18 +34,18 @@ export class DynamicImagePanel extends PureComponent<Props> {
       )
     }
 
-    let urls: string[] = []
+    let names: string[] = []
     for (const tmp of data.series) {
       for (const tmp2 of tmp.fields) {
         if (tmp2.name == options.field) {
           for (const name of tmp2.values.toArray()) {
-            urls.push(options.baseUrl + name + options.suffix)
+            names.push(name)
           }
         }
       }
     }
 
-    if (!urls || urls.length == 0) {
+    if (!names || names.length == 0) {
       return (
         <div className="panel-empty">
           <p>No data found in response</p>
@@ -53,19 +53,19 @@ export class DynamicImagePanel extends PureComponent<Props> {
       )
     }
 
-    if (options.singleFill && urls.length == 1) {
+    if (options.singleFill && names.length == 1) {
       return (
         <div className="image-container full">
-          <img src={urls[0]} />
+          <img src={options.baseUrl + names[0] + options.suffix} alt={names[0]} />
         </div>
       )
     }
-    //var url = options.baseUrl + icon + options.suffix
+
     return (
       <div className="container">
-        {urls.map(url => {
+        {names.map(name => {
           return (
-            <img src={url} style={{ width: options.width + "px", height: options.height + "px" }} />
+            <img src={options.baseUrl + name + options.suffix} style={{ width: options.width + "px", height: options.height + "px" }} alt={name} />
           )
         })}
       </div>
