ff709bd2d9c1598c6bf6d3b5550b65f781fd73b6
Dalvany <Dalvany@users.noreply.github.com>
2020-10-24 17:54:45 +0200

    Add logging
    Log dataframes for debug and add logging message in case of error

--- src/DynamicImagePanel.tsx
+++ src/DynamicImagePanel.tsx
@@ -1,10 +1,16 @@
 import React, {PureComponent} from 'react';
-import {dateTimeFormat, dateTimeFormatTimeAgo, Field, FieldType, PanelProps} from '@grafana/data';
+import {DataFrame, dateTimeFormat, dateTimeFormatTimeAgo, Field, FieldType, PanelProps} from '@grafana/data';
 import {DynamicImageOptions} from './types';
 // @ts-ignore
 import './css/image.css';
 
 interface Props extends PanelProps<DynamicImageOptions> {
+}
+
+export interface Value {
+    icon: string,
+    alt: string,
+    tooltip: string
 }
 
 export class DynamicImagePanel extends PureComponent<Props> {
@@ -33,9 +39,28 @@ export class DynamicImagePanel extends PureComponent<Props> {
         return -1;
     }
 
+    logDataFrame(dataframe: DataFrame[]) {
+        for (let i = 0; i < dataframe.length; i++) {
+            const df = dataframe[i];
+            console.debug("serie" + i + " " + df.name)
+            for (let j = 0; j < df.fields.length; j++) {
+                const field = df.fields[j]
+                console.debug("field" + j + " : " + field.name + " " + field.type + " (contains " + field.values.length + " values)")
+            }
+        }
+    }
+
+    logImageData(values: Value[], start: string, end: string, tooltip: boolean) {
+        for (let i = 0; i < values.length; i++) {
+            const v: Value = values[i]
+            console.debug("Data" + i + " : url='" + start + v.icon + end + "', alt='" + v.alt + "', tooltip=" + (tooltip ? "'" + v.tooltip + "'" : "disabled"))
+        }
+    }
+
     render() {
         const {options, data} = this.props;
         if (!data || data.series.length == 0) {
+            console.error("data is empty or null")
             return (
                 <div className="panel-empty">
                     <p>No data found in response.</p>
@@ -43,16 +68,22 @@ export class DynamicImagePanel extends PureComponent<Props> {
             )
         }
         if (data.error) {
-            console.error(data.error.message)
+            console.error("Message : " + data.error.message)
             return (
                 <div className="panel-empty">
-                    <p>Error.</p>
+                    <p>Error. See javascript console for more precision</p>
                 </div>
             )
         }
 
+        this.logDataFrame(data.series)
+
         const number_series = data.series.length;
         if (number_series > 1) {
+            console.error(data.series.length + " timeseries (see series in debug)")
+            for (let i = 0; i < data.series.length; i++) {
+                console.error("Serie " + i + " : " + data.series[i].name)
+            }
             return (
                 <div className="panel-empty">
                     <p>There's multiple time series. Use the outer join transform.</p>
@@ -67,12 +98,14 @@ export class DynamicImagePanel extends PureComponent<Props> {
         let icon_index = this.getFieldIndex(options.icon_field, data.series[0].fields);
         if (icon_index == -1) {
             if (options.icon_field == '') {
+                console.error("Missing non time field from data (see series in debug)")
                 return (
                     <div className="panel-empty">
                         <p>Can't find a non time field for image.</p>
                     </div>
                 )
             } else {
+                console.error("Missing field '" + options.icon_field + "' from data (see series in debug)")
                 return (
                     <div className="panel-empty">
                         <p>Can't find {options.icon_field} field for image.</p>
@@ -84,6 +117,7 @@ export class DynamicImagePanel extends PureComponent<Props> {
         // Find alt field
         let alt_index = options.alt_field == '' ? icon_index : this.getFieldIndex(options.alt_field, data.series[0].fields);
         if (alt_index == -1) {
+            console.error("Missing field '" + options.alt_field + "' from data for alt (see series in debug)")
             return (
                 <div className="panel-empty">
                     <p>Can't find {options.alt_field} field for alt.</p>
@@ -98,6 +132,7 @@ export class DynamicImagePanel extends PureComponent<Props> {
             ? this.getFieldIndex(options.tooltip_field, data.series[0].fields)
             : icon_index;
         if (tooltip_index == -1) {
+            console.error("Missing field '" + options.tooltip_field + "' from data for tooltip (see series in debug)")
             return (
                 <div className="panel-empty">
                     <p>Can't find {options.tooltip_field} field for tooltip.</p>
@@ -108,6 +143,7 @@ export class DynamicImagePanel extends PureComponent<Props> {
         // Find time field for tooltip (if no tooltip or if it doesn't include time, use icon field as we don't care about values)
         let time_index = options.tooltip && options.tooltip_include_date ? this.getTimeFieldIndex(data.series[0].fields) : icon_index;
         if (time_index == -1) {
+            console.error("Missing time field from data for tooltip (see series in debug)")
             return (
                 <div className="panel-empty">
                     <p>Can't find time field for tooltip.</p>
@@ -115,7 +151,7 @@ export class DynamicImagePanel extends PureComponent<Props> {
             )
         }
 
-        let values = [] as any
+        let values: Value[] = []
         for (let i = 0; i < max; i++) {
             let t = "";
             if (options.tooltip && options.tooltip_include_date) {
@@ -138,16 +174,18 @@ export class DynamicImagePanel extends PureComponent<Props> {
             });
         }
 
+        let start = options.baseUrl === undefined ? "" : options.baseUrl;
+        let end = options.suffix === undefined ? "" : options.suffix
+        this.logImageData(values, start, end, options.tooltip)
+
         if (!values || values.length == 0) {
+            console.error("Serie contains no values (see series in debug)")
             return (
                 <div className="panel-empty">
                     <p>No data found in response.</p>
                 </div>
             )
         }
-
-        let start = options.baseUrl === undefined ? "":options.baseUrl;
-        let end = options.suffix === undefined ? "":options.suffix
 
         if (options.singleFill && values.length == 1) {
             if (options.tooltip) {
