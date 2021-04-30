1cdffe3b93dfb3bb60a5dd6dfa680afef0abc591
Dalvany <9901407+Dalvany@users.noreply.github.com>
2021-04-30 22:31:47 +0200

    Start of binding options

--- src/OverlayConfigEditor.tsx
+++ src/OverlayConfigEditor.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { StandardEditorProps, SelectableValue } from '@grafana/data';
-import { Input, RadioButtonGroup } from '@grafana/ui';
+import { Button, ColorPicker, Icon, Input, RadioButtonGroup } from '@grafana/ui';
 
 export interface Size {
   size: number;
   unit: string;
+}
+
+export interface EditorProps {
+  binding: Bindings;
+  onChange: (value?: Bindings) => void;
+}
+
+export interface Bindings {
+  bindings: Binding[];
+}
+
+export interface Binding {
+  color: string;
+  value?: string | number;
 }
 
 export const SizeEditor: React.FC<StandardEditorProps<Size>> = ({ value, onChange }) => {
@@ -51,4 +65,81 @@ export const SizeEditor: React.FC<StandardEditorProps<Size>> = ({ value, onChang
       />
     </div>
   );
+};
+
+class BindingComponentEditor extends PureComponent<EditorProps> {
+  onChangeColor = (index: number, color: string) => {
+    const { binding } = this.props;
+    binding.bindings[index] = {
+      color: color,
+    };
+    this.forceUpdate();
+  };
+
+  addBinding = () => {
+    const { binding, onChange } = this.props;
+
+    let n: Binding[] = [];
+    binding.bindings.map(function (b) {
+      n.push(b);
+    });
+
+    n.push({
+      color: '#AAAAAA',
+    });
+    onChange({
+      bindings: n,
+    });
+    this.forceUpdate();
+  };
+
+  removeBinding = (index: number) => {
+    const { binding, onChange } = this.props;
+
+    binding.bindings.splice(index, 1);
+    onChange({
+      bindings: binding.bindings,
+    });
+    this.forceUpdate();
+  };
+
+  render() {
+    const { binding } = this.props;
+
+    let d: JSX.Element[] = [];
+    let remove_function: (index: number) => void = this.removeBinding;
+    let change_function: (index: number, color: string) => void = this.onChangeColor;
+    if (binding.bindings !== undefined) {
+      d = binding.bindings.map(function (b, idx) {
+        let prefix = (
+          <div>
+            <ColorPicker color={b.color} onChange={(color) => change_function(idx, color)} />
+          </div>
+        );
+        let suffix = <Icon className={'trash-alt'} name="trash-alt" onClick={() => remove_function(idx)} />;
+        return <Input type={'text'} prefix={prefix} suffix={suffix} />;
+      });
+    }
+
+    return (
+      <div>
+        <div>{d}</div>
+        <div>
+          <Button className={'css-stylot-button'} onClick={this.addBinding}>
+            Add binding
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export const BindingEditor: React.FC<StandardEditorProps<Bindings>> = ({ value, onChange }) => {
+  let v: Bindings = value;
+  if (v === undefined) {
+    v = {
+      bindings: [],
+    };
+  }
+  return <BindingComponentEditor binding={v} onChange={onChange} />;
 };
--- src/module.tsx
+++ src/module.tsx
@@ -7,8 +7,7 @@ import {
 } from '@grafana/data';
 import { DynamicImageOptions, Position } from './types';
 import { DynamicImagePanel } from './DynamicImagePanel';
-import { SizeEditor } from './OverlayConfigEditor';
-import { ColorBindingEditor } from './ColorBinding';
+import { BindingEditor, SizeEditor } from './OverlayConfigEditor';
 
 function listFields(context: FieldOverrideContext, first?: any) {
   const options = [first] as any;
@@ -55,198 +54,194 @@ function findFirstNonTimeField(context: FieldOverrideContext) {
   return {};
 }
 
-export const plugin = new PanelPlugin<DynamicImageOptions>(DynamicImagePanel)
-  .useFieldConfig()
-  .setPanelOptions((builder) => {
-    // Instead of using builder, use custom editor ? It will allow to select the first field in the 'select' component
-    // https://github.com/grafana/grafana/blob/master/public/app/core/components/TransformersUI/SeriesToFieldsTransformerEditor.tsx
-    // https://github.com/grafana/grafana/blob/master/public/app/plugins/panel/stat/types.ts#L65
-    return builder
-      .addTextInput({
-        path: 'baseUrl',
-        name: 'Base URL',
-        description: 'First part of the URL',
-        category: ['URL'],
-      })
-      .addSelect({
-        path: 'icon_field',
-        name: 'Icon field',
-        description: 'Field value to use in the URL',
-        settings: {
-          allowCustomValue: false,
-          options: [],
-          getOptions: async (context: FieldOverrideContext) => {
-            // From https://github.com/grafana/grafana/blob/d526647005d8fcba85a02b4de70a10c689726d16/public/app/plugins/panel/stat/types.ts#L89
-            return Promise.resolve(listFields(context, { value: '', label: 'First non time field' }));
-          },
+export const plugin = new PanelPlugin<DynamicImageOptions>(DynamicImagePanel).setPanelOptions((builder) => {
+  // Instead of using builder, use custom editor ? It will allow to select the first field in the 'select' component
+  // https://github.com/grafana/grafana/blob/master/public/app/core/components/TransformersUI/SeriesToFieldsTransformerEditor.tsx
+  // https://github.com/grafana/grafana/blob/master/public/app/plugins/panel/stat/types.ts#L65
+  return builder
+    .addTextInput({
+      path: 'baseUrl',
+      name: 'Base URL',
+      description: 'First part of the URL',
+      category: ['URL'],
+    })
+    .addSelect({
+      path: 'icon_field',
+      name: 'Icon field',
+      description: 'Field value to use in the URL',
+      settings: {
+        allowCustomValue: false,
+        options: [],
+        getOptions: async (context: FieldOverrideContext) => {
+          // From https://github.com/grafana/grafana/blob/d526647005d8fcba85a02b4de70a10c689726d16/public/app/plugins/panel/stat/types.ts#L89
+          return Promise.resolve(listFields(context, { value: '', label: 'First non time field' }));
         },
-        // I don't know how I can get the first non time field here, so I put a
-        // "First non time value" on options in 'listFields' function and default
-        // to this 'special' value
-        defaultValue: '',
-        category: ['URL'],
-      })
-      .addTextInput({
-        path: 'suffix',
-        name: 'Suffix',
-        description: 'To append at the end of the URL',
-        category: ['URL'],
-      })
-      .addTextInput({
-        path: 'width',
-        name: 'Image width',
-        description: "Image width in pixel (potentially ignored if 'single fill')",
-        defaultValue: '75',
-        category: ['Image options'],
-      })
-      .addTextInput({
-        path: 'height',
-        name: 'Image height',
-        description: "Image height in pixel (potentially ignored if 'single fill')",
-        defaultValue: '75',
-        category: ['Image options'],
-      })
-      .addBooleanSwitch({
-        path: 'singleFill',
-        name: 'Single fill',
-        description: 'If there is a single image, it will try to fill panel',
-        defaultValue: true,
-        category: ['Image options'],
-      })
-      .addSelect({
-        path: 'alt_field',
-        name: 'Alt field',
-        description: "Field value that is displayed if image doesn't exists",
-        settings: {
-          allowCustomValue: false,
-          options: [],
-          getOptions: async (context: FieldOverrideContext) => {
-            return Promise.resolve(listFields(context, { value: '', label: 'Use icon field' }));
-          },
+      },
+      // I don't know how I can get the first non time field here, so I put a
+      // "First non time value" on options in 'listFields' function and default
+      // to this 'special' value
+      defaultValue: '',
+      category: ['URL'],
+    })
+    .addTextInput({
+      path: 'suffix',
+      name: 'Suffix',
+      description: 'To append at the end of the URL',
+      category: ['URL'],
+    })
+    .addTextInput({
+      path: 'width',
+      name: 'Image width',
+      description: "Image width in pixel (potentially ignored if 'single fill')",
+      defaultValue: '75',
+      category: ['Image options'],
+    })
+    .addTextInput({
+      path: 'height',
+      name: 'Image height',
+      description: "Image height in pixel (potentially ignored if 'single fill')",
+      defaultValue: '75',
+      category: ['Image options'],
+    })
+    .addBooleanSwitch({
+      path: 'singleFill',
+      name: 'Single fill',
+      description: 'If there is a single image, it will try to fill panel',
+      defaultValue: true,
+      category: ['Image options'],
+    })
+    .addSelect({
+      path: 'alt_field',
+      name: 'Alt field',
+      description: "Field value that is displayed if image doesn't exists",
+      settings: {
+        allowCustomValue: false,
+        options: [],
+        getOptions: async (context: FieldOverrideContext) => {
+          return Promise.resolve(listFields(context, { value: '', label: 'Use icon field' }));
         },
-        defaultValue: '',
-        category: ['Image options'],
-      })
-      .addBooleanSwitch({
-        path: 'tooltip',
-        name: 'Include tooltip',
-        description: 'Image have a tooltip',
-        defaultValue: false,
-        category: ['Image tooltip options'],
-      })
-      .addBooleanSwitch({
-        path: 'tooltip_include_field',
-        name: 'Include field',
-        description: 'Include a field value in tooltip text',
-        defaultValue: true,
-        showIf: (currentConfig) => currentConfig.tooltip,
-        category: ['Image tooltip options'],
-      })
-      .addSelect({
-        path: 'tooltip_field',
-        name: 'Tooltip field',
-        description: 'Field value, if any, to include in the tooltip text',
-        settings: {
-          allowCustomValue: false,
-          options: [],
-          getOptions: async (context: FieldOverrideContext) => {
-            return Promise.resolve(listFields(context, { value: '', label: 'Use icon field' }));
-          },
+      },
+      defaultValue: '',
+      category: ['Image options'],
+    })
+    .addBooleanSwitch({
+      path: 'tooltip',
+      name: 'Include tooltip',
+      description: 'Image have a tooltip',
+      defaultValue: false,
+      category: ['Image tooltip options'],
+    })
+    .addBooleanSwitch({
+      path: 'tooltip_include_field',
+      name: 'Include field',
+      description: 'Include a field value in tooltip text',
+      defaultValue: true,
+      showIf: (currentConfig) => currentConfig.tooltip,
+      category: ['Image tooltip options'],
+    })
+    .addSelect({
+      path: 'tooltip_field',
+      name: 'Tooltip field',
+      description: 'Field value, if any, to include in the tooltip text',
+      settings: {
+        allowCustomValue: false,
+        options: [],
+        getOptions: async (context: FieldOverrideContext) => {
+          return Promise.resolve(listFields(context, { value: '', label: 'Use icon field' }));
         },
-        defaultValue: '',
-        showIf: (currentConfig) => currentConfig.tooltip && currentConfig.tooltip_include_field,
-        category: ['Image tooltip options'],
-      })
-      .addBooleanSwitch({
-        path: 'tooltip_include_date',
-        name: 'Include date',
-        description: 'Include the date, if any, in tooltip text',
-        defaultValue: false,
-        showIf: (currentConfig) => currentConfig.tooltip,
-        category: ['Image tooltip options'],
-      })
-      .addBooleanSwitch({
-        path: 'tooltip_date_elapsed',
-        name: 'As elapsed',
-        description: 'Display as elapsed date',
-        defaultValue: false,
-        showIf: (currentConfig) => currentConfig.tooltip && currentConfig.tooltip_include_date,
-        category: ['Image tooltip options'],
-      })
-      .addBooleanSwitch({
-        path: 'show_overlay',
-        name: 'Show overlay',
-        description: 'Display a small colored square on the corner of pictures',
-        defaultValue: false,
-        category: ['Overlay'],
-      })
-      .addSelect({
-        path: 'overlay.position',
-        name: 'Position',
-        description: 'Position of the overlay',
-        defaultValue: Position.TOP_RIGHT,
-        settings: {
-          allowCustomValue: false,
-          options: [
-            { value: Position.TOP_LEFT, label: Position.TOP_LEFT },
-            { value: Position.TOP_RIGHT, label: Position.TOP_RIGHT },
-            { value: Position.BOTTOM_LEFT, label: Position.BOTTOM_LEFT },
-            { value: Position.BOTTOM_RIGHT, label: Position.BOTTOM_RIGHT },
-          ],
+      },
+      defaultValue: '',
+      showIf: (currentConfig) => currentConfig.tooltip && currentConfig.tooltip_include_field,
+      category: ['Image tooltip options'],
+    })
+    .addBooleanSwitch({
+      path: 'tooltip_include_date',
+      name: 'Include date',
+      description: 'Include the date, if any, in tooltip text',
+      defaultValue: false,
+      showIf: (currentConfig) => currentConfig.tooltip,
+      category: ['Image tooltip options'],
+    })
+    .addBooleanSwitch({
+      path: 'tooltip_date_elapsed',
+      name: 'As elapsed',
+      description: 'Display as elapsed date',
+      defaultValue: false,
+      showIf: (currentConfig) => currentConfig.tooltip && currentConfig.tooltip_include_date,
+      category: ['Image tooltip options'],
+    })
+    .addBooleanSwitch({
+      path: 'show_overlay',
+      name: 'Show overlay',
+      description: 'Display a small colored square on the corner of pictures',
+      defaultValue: false,
+      category: ['Overlay'],
+    })
+    .addSelect({
+      path: 'overlay.position',
+      name: 'Position',
+      description: 'Position of the overlay',
+      defaultValue: Position.TOP_RIGHT,
+      settings: {
+        allowCustomValue: false,
+        options: [
+          { value: Position.TOP_LEFT, label: Position.TOP_LEFT },
+          { value: Position.TOP_RIGHT, label: Position.TOP_RIGHT },
+          { value: Position.BOTTOM_LEFT, label: Position.BOTTOM_LEFT },
+          { value: Position.BOTTOM_RIGHT, label: Position.BOTTOM_RIGHT },
+        ],
+      },
+      showIf: (currentConfig) => currentConfig.show_overlay,
+      category: ['Overlay'],
+    })
+    .addCustomEditor({
+      editor: SizeEditor,
+      id: 'overlay.width',
+      name: 'Width',
+      path: 'overlay.width',
+      description: 'Width of the overlay',
+      defaultValue: {
+        size: 5,
+        unit: '%',
+      },
+      showIf: (currentConfig) => currentConfig.show_overlay,
+      category: ['Overlay'],
+    })
+    .addCustomEditor({
+      editor: SizeEditor,
+      id: 'overlay.height',
+      name: 'Height',
+      path: 'overlay.height',
+      description: 'Height of the overlay',
+      defaultValue: {
+        size: 5,
+        unit: '%',
+      },
+      showIf: (currentConfig) => currentConfig.show_overlay,
+      category: ['Overlay'],
+    })
+    .addSelect({
+      path: 'overlay.field',
+      name: 'Overlay field',
+      description: 'Field to use for color mapping',
+      settings: {
+        allowCustomValue: false,
+        options: [],
+        getOptions: async (context: FieldOverrideContext) => {
+          return Promise.resolve(listFieldsNew(context));
         },
-        showIf: (currentConfig) => currentConfig.show_overlay,
-        category: ['Overlay'],
-      })
-      .addCustomEditor({
-        editor: SizeEditor,
-        id: 'overlay.width',
-        name: 'Width',
-        path: 'overlay.width',
-        description: 'Width of the overlay',
-        defaultValue: {
-          size: 5,
-          unit: '%',
-        },
-        showIf: (currentConfig) => currentConfig.show_overlay,
-        category: ['Overlay'],
-      })
-      .addCustomEditor({
-        editor: SizeEditor,
-        id: 'overlay.height',
-        name: 'Height',
-        path: 'overlay.height',
-        description: 'Height of the overlay',
-        defaultValue: {
-          size: 5,
-          unit: '%',
-        },
-        showIf: (currentConfig) => currentConfig.show_overlay,
-        category: ['Overlay'],
-      })
-      .addSelect({
-        path: 'overlay.field',
-        name: 'Overlay field',
-        description: 'Field to use for color mapping',
-        settings: {
-          allowCustomValue: false,
-          options: [],
-          getOptions: async (context: FieldOverrideContext) => {
-            return Promise.resolve(listFieldsNew(context));
-          },
-        },
-        showIf: (currentConfig) => currentConfig.show_overlay,
-        category: ['Overlay'],
-      })
-      .addCustomEditor({
-        id: 'overlay.thresholds',
-        path: 'overlay.thresholds',
-        name: 'Thresholds/binding',
-        description: 'Set thresholds for binding overlay color',
-        editor: ColorBindingEditor,
-        showIf: (currentConfig) =>
-          currentConfig.show_overlay &&
-          currentConfig.overlay !== undefined &&
-          currentConfig.overlay.field !== undefined,
-        category: ['Overlay'],
-      });
-  });
+      },
+      showIf: (currentConfig) => currentConfig.show_overlay,
+      category: ['Overlay'],
+    })
+    .addCustomEditor({
+      id: 'overlay.thresholds',
+      path: 'overlay.thresholds',
+      name: 'Binding',
+      description: 'Set binding (act as threshold if values are numbers) overlay color',
+      editor: BindingEditor,
+      showIf: (currentConfig) =>
+        currentConfig.show_overlay && currentConfig.overlay !== undefined && currentConfig.overlay.field !== undefined,
+      category: ['Overlay'],
+    });
+});
